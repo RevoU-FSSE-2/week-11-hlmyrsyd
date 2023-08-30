893b6ea39933badbf189f05278f303f479a25259
jodyboy <elite.crew66@gmail.com>
2023-08-30 19:28:16 +0700

    Preparation Updated

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const swaggerUi = require('swagger-ui-express');
-const path = require('path');
+// const path = require('path');
 const yaml = require('yaml');
 const fs = require('fs');
 
@@ -11,18 +11,36 @@ const swaggerDocument = yaml.parse(file);
 const app = express();
 const tasks = require('./routes/tasks')
 
+// DB
+const connectDB = require('./db/connect')
+require('dotenv').config()
+
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI)
+
+        // LISTEN 
+        app.listen(port, () => {
+            console.log(`test on ${port}`);
+        });
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start()
+
 // PORT 
 const port = 3033
 
 // SERVE SWAGGER UI
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
+// MIDDLEWARE
+
+app.use(express.json())
+
 // ROUTES
 
 app.use('/api/v1/tasks', tasks)
 
-// LISTEN
-
-app.listen(port, () => {
-    console.log(`test on ${port}`);
-});
